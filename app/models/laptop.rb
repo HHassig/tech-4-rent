38721a813bf2047eb547a4887c1ee1584c0430a9
Harrison Hassig <harrisonhassig@gmail.com>
2022-11-21 16:10:27 +0100

    Laptop and booking model made

--- app/models/laptop.rb
+++ app/models/laptop.rb
@@ -1,6 +1,4 @@
 class Laptop < ApplicationRecord
   belongs_to :user
-
-  has_many :reviews
-  validates :hard_drive, :ram, :operating_system, :screen_size, :year_built, presence: true
+  has_many :bookings
 end
