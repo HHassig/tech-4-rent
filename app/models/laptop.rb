96c4cfdadb086dec92896d7f3f981cb4370e7f07
Harrison Hassig <harrisonhassig@gmail.com>
2022-11-21 16:03:51 +0100

    devise-gem comes first

--- app/models/laptop.rb
+++
@@ -1,7 +0,0 @@
-class Laptop < ApplicationRecord
-  belongs_to :user
-  belongs_to :booking
-
-  has_many :reviews
-  validates :hard_drive, :ram, :operating_system, :screen_size, :year_built, presence: true
-end
