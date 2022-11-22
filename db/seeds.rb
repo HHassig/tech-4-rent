21e35b3a682c5a5a3234be810d721b6cedede75c
Sergi Escarpenter <sergiescarpenter@gmail.com>
2022-11-22 18:08:39 +0100

    Finish seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "csv"
+require "open-uri"
+
+csv_text = File.read(Rails.root.join('lib','seeds','users.csv'))
+csv = CSV.parse(csv_text, headers: true, encoding: 'UTF-8')
+
+csv.each do |row|
+  file = URI.open(row["profile_picture"])
+  puts "opened url #{row["profile_picture"]}"
+  user = User.new({ first_name: row["first_name"], last_name: row["last_name"], email: row["email"], password: row["password"], username: row["username"], user_type: row["user_type"] })
+  puts "user.new #{user.id} done"
+  user.profile_picture.attach(io: file, content_type: "image/png", filename: "#{user.id}.jpg")
+  puts "attached photo"
+  user.save!
+  puts "saved"
+end
+
+csv_text = File.read(Rails.root.join('lib','seeds','laptops.csv'))
+csv = CSV.parse(csv_text, headers: true, encoding: 'UTF-8')
+
+csv.each do |row|
+  file = URI.open(row["photo"])
+  puts "opened url #{row["photo"]}"
+  laptop = Laptop.new({ year_built: row["year_built"], brand: row["brand"], model: row["model"], screen_size: row["screen_size"], hard_drive: row["hard_drive"], ram: row["ram"], user_id: row["user"].to_i, price: row["price"] })
+  puts "laptop.new done"
+  laptop.photo.attach(io: file, content_type: "image/png", filename: "laptop#{laptop.id}.jpg")
+  puts "attached photo"
+  laptop.save!
+  puts "saved"
+end
+
+
+csv_text = File.read(Rails.root.join('lib','seeds','bookings.csv'))
+csv = CSV.parse(csv_text, headers: true, encoding: 'UTF-8')
+
+csv.each do |row|
+  booking = Booking.new({ user_id: row["user"], laptop_id: row["laptop_id"].to_i, duration: row["duration"] })
+  puts "booking.new done"
+  booking.save!
+  puts "saved"
+end
