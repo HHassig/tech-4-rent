f67f1ecb16e4e8907dc693d6e344a116ac13c1e1
Harrison Hassig <harrisonhassig@gmail.com>
2022-11-24 17:52:29 +0100

    Given each laptop an avg_rating

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_24_140958) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_163731) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,6 +66,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_140958) do
     t.float "latitude"
     t.float "longitude"
     t.string "address"
+    t.float "reviews"
+    t.float "average_rating"
     t.index ["user_id"], name: "index_laptops_on_user_id"
   end
 
