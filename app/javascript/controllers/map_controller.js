c61080e2c43d2401b4969ab382a1751aaa576f17
Harrison Hassig <harrisonhassig@gmail.com>
2022-11-24 11:32:50 +0100

    Still no markers

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,4 +1,3 @@
-// app/javascript/controllers/map_controller.js
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
@@ -6,25 +5,23 @@ export default class extends Controller {
     apiKey: String,
     markers: Array
   }
+
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
+
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10"
-    });
-  this.#addMarkersToMap()
-  this.#fitMapToMarkers()
+    })
+
+    this.#addMarkersToMap()
   }
-  #fitMapToMarkers() {
-    const bounds = new mapboxgl.LngLatBounds()
-    this.markersValue.forEach(marker => { bounds.extend([ marker.lng, marker.lat ]) });
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 10 })
-  }
+
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
         .addTo(this.map)
-    });
+    })
   }
 }
