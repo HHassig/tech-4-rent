635c7a6a249002f487a9ab0937788d9231e2cac0
Harrison Hassig <harrisonhassig@gmail.com>
2022-11-24 12:12:08 +0100

    Geocoded

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,5 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
+// import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 
+// Connects to data-controller="mapbox"
 export default class extends Controller {
   static values = {
     apiKey: String,
@@ -11,10 +13,11 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/pdunleav/cjofefl7u3j3e2sp0ylex3cyb"
     })
-
     this.#addMarkersToMap()
+    this.#fitMapToMarkers()
+
   }
 
   #addMarkersToMap() {
@@ -23,5 +26,11 @@ export default class extends Controller {
         .setLngLat([ marker.lng, marker.lat ])
         .addTo(this.map)
     })
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
 }
