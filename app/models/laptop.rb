abfb969454688142def38fcf639aeefc2d90c4a9
Harrison Hassig <harrisonhassig@gmail.com>
2022-11-25 12:13:05 +0100

    Default average rating

--- app/models/laptop.rb
+++ app/models/laptop.rb
@@ -1,7 +1,14 @@
 class Laptop < ApplicationRecord
+  after_initialize :set_defaults
   belongs_to :user
   has_one_attached :photo
   has_many :reviews
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  private
+
+  def set_defaults
+    self.average_rating ||= 0.0
+  end
 end
